c56f9bf3ed35b51a1436f0c4381b4d0d453b69e2
radicspeter1975 <radicsp75@gmail.com>
2026-07-01 15:57:52 +0200

    new post

--- zenelde/zenelde.js
+++ zenelde/zenelde.js
@@ -1391,5 +1391,6 @@ const listaT = [
 ["4oCCxX3OjRo", "2026-06-20", "Moby Dick - Beteg a Föld"],
 ["8B9elM8fw-Q", "2026-06-22", "Kispál és a Borz - Tingli-tangli (+mellékdal)"],
 ["zWwOO3hXPZA", "2026-06-23", "Fun Lovin' Criminals - Big Night Out"],
-["pZLIWoUbcyo", "2026-06-30", "Die Toten Hosen - Azzurro #cover"]
+["pZLIWoUbcyo", "2026-06-30", "Die Toten Hosen - Azzurro #cover"],
+["C6dpQWnRII0", "2026-07-01", "Eros Ramazzotti - Se bastasse una canzone"]
 ];
